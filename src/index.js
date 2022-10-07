09b8ef9eeaec94b7ed5b0d7d108ea66520b12903
shihangbo <shihangbo@126.com>
2022-10-07 19:38:10 +0800

    test

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import {Provider} from 'react-redux'
 import store from './store/index'
 import './assets/css/base.css'
 
-console.log('imou-sensors init')
-console.log('imou-sensors init')
+console.log('imou-sensors init  sensors.init')
 
 ReactDOM.render(
   <Provider store={store}>
